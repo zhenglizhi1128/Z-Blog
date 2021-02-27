55bb41c92e305d024976689e4eb62792e2555350
zhenglizhi1128 <17519148828@163.com>
2021-02-27 10:39:42 +0800

    修改配置

---
+++ src/test/java/com/zhenglz/mapper/BlogMapperTest.java
@@ -0,0 +1,30 @@
+package com.zhenglz.mapper;
+
+import com.zhenglz.entity.Blog;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+/**
+ * @Auther: zlz
+ * @Date: 2021/02/27/10:32
+ * @Description:
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogMapperTest {
+
+    @Autowired
+    BlogMapper blogMapper;
+
+    @Test
+    public void list() {
+        List<Blog> list = blogMapper.list();
+        System.out.println(list.size());
+        System.out.println("------");
+    }
+}
---
+++ src/test/java/com/zhenglz/utils/JsonTest.java
@@ -0,0 +1,36 @@
+package com.zhenglz.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zhenglz.entity.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JsonTest {
+
+    public static Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
+
+    private User user;
+
+
+
+    @Test
+    public void loggerTest()  {
+      /*  ArrayNode jsonArray = JsonUtils.createJsonArray();
+        ObjectNode json = JsonUtils.createJson();
+        json.put("username","张三");
+        ObjectNode json1 = JsonUtils.createJson();
+        json1.put("username","张三1");
+        jsonArray.add(json).add(json1);*/
+
+        System.out.println(JsonUtils.jsonStrToJson(""));
+        ObjectMapper objectMapper = JsonUtils.getObjectMapper();
+
+    }
+
+}
