c1c3d3b65381358f7d24a72bf196138d66c4b92c
zhenglizhi1128 <17519148828@163.com>
2021-03-19 15:15:52 +0800

    修改数据库

--- src/main/resources/Z-Blog.sql
+++ src/main/resources/Z-Blog.sql
@@ -1,36 +1,38 @@
+CREATE DATABASE IF NOT EXISTS zblog DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+
 DROP TABLE IF EXISTS `m_blog`;
 CREATE TABLE `m_blog` (
       `id` bigint(40) NOT NULL AUTO_INCREMENT COMMENT '主键',
       `user_id` bigint(40) NOT NULL DEFAULT '0' COMMENT '用户Id',
       `title` varchar(255) NOT NULL DEFAULT '' COMMENT '标题',
       `description` varchar(255) NOT NULL DEFAULT '' COMMENT '摘要',
-      `content` longtext NOT NULL COMMENT '内容',
+      `content_id` bigint(40) NOT NULL DEFAULT '0' COMMENT '内容Id',
       `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
       `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+      `read_number` bigint(40) NOT NULL DEFAULT '0' COMMENT '阅读数',
+      `like_number` bigint(40) NOT NULL DEFAULT '0' COMMENT '点赞数',
+      `comment_number` bigint(40) NOT NULL DEFAULT '0' COMMENT '评论数',
       `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '状态',
+      `comment_status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '评论状态',
       PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COMMENT '博客信息';
+) ENGINE=InnoDB CHARSET=utf8 COMMENT '博客信息';
+
+
+CREATE TABLE `m_blog_content` (
+      `id` bigint(40) NOT NULL AUTO_INCREMENT COMMENT '主键',
+      `content` longtext NOT NULL COMMENT '内容',
+      `blog_id` bigint(40) NOT NULL DEFAULT '0' COMMENT '对应文章ID',
+      PRIMARY KEY (`id`),
+      UNIQUE KEY `blog_id` (`blog_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '博客内容';
 
 -- ----------------------------
 -- Records of m_blog
 -- ----------------------------
 INSERT INTO `m_blog`
-VALUES ('1', '1', '生活就像海洋，只有意志坚强的人才能到达彼岸', '这里是摘要哈哈哈', '内容？？？', now(),now(),'0');
-INSERT INTO `m_blog`
-VALUES ('2', '1', '最值得学习的博客项目',
-        'blog是一个基于Springboot2.1.2开发的博客学习项目，为了让项目融合更多的知识点，达到学习目的，编写了详细的从0到1开发文档。主要学习包括：自定义Freemarker标签，使用shiro+redis完成了会话共享，redis的set结构完成本周热议排行榜，t-io+websocket完成即时消息通知和群聊，rabbitmq+elasticsearch完成博客内容搜索引擎等。值得学习的地方很多！',
-        '**推荐阅读：**\r\n\r\n[分享一套SpringBoot开发博客系统源码，以及完整开发文档！速度保存！](https://mp.weixin.qq.com/s/jz6e977xP-OyaAKNjNca8w)\r\n\r\n[Github上最值得学习的100个Java开源项目，涵盖各种技术栈！](https://mp.weixin.qq.com/s/N-U0TaEUXnBFfBsmt_OESQ)\r\n\r\n[2020年最新的常问企业面试题大全以及答案](https://mp.weixin.qq.com/s/lR5LC5GnD2Gs59ecV5R0XA)',
-        now(),now(), '0');
-INSERT INTO `m_blog`
-VALUES ('7', '1', '你真的会写单例模式吗?',
-        '单例模式可能是代码最少的模式了，但是少不一定意味着简单，想要用好、用对单例模式，还真得费一番脑筋。本文对 Java 中常见的单例模式写法做了一个总结，如有错漏之处，恳请读者指正。',
-        '> 作者：吃桔子的攻城狮 来源：http://www.tekbroaden.com/singleton-java.html\n\n\n单例模式可能是代码最少的模式了，但是少不一定意味着简单，想要用好、用对单例模式，还真得费一番脑筋。本文对 Java 中常见的单例模式写法做了一个总结，如有错漏之处，恳请读者指正。\n\n饿汉法\n===\n\n顾名思义，饿汉法就是在第一次引用该类的时候就创建对象实例，而不管实际是否需要创建。代码如下：\n\n```\npublic class Singleton {  \n    private static Singleton = new Singleton();\n    private Singleton() {}\n    public static getSingleton(){\n        return singleton;\n    }\n}\n\n```\n\n这样做的好处是编写简单，但是无法做到延迟创建对象。但是我们很多时候都希望对象可以尽可能地延迟加载，从而减小负载，所以就需要下面的懒汉法：\n',
-        now(),now(), '0');
-INSERT INTO `m_blog`
-VALUES ('9', '1', '真正理解Mysql的四种隔离级别@',
-        '事务是应用程序中一系列严密的操作，所有操作必须成功完成，否则在每个操作中所作的所有更改都会被撤消。也就是事务具有原子性，一个事务中的一系列的操作要么全部成功，要么一个都不做。\n\n事务的结束有两种，当事务中的所以步骤全部成功执行时，事务提交。如果其中一个步骤失败，将发生回滚操作，撤消撤消之前到事务开始时的所以操作。',
-        '### 什么是事务  \n\n> 事务是应用程序中一系列严密的操作，所有操作必须成功完成，否则在每个操作中所作的所有更改都会被撤消。也就是事务具有原子性，一个事务中的一系列的操作要么全部成功，要么一个都不做。\n> \n> 事务的结束有两种，当事务中的所以步骤全部成功执行时，事务提交。如果其中一个步骤失败，将发生回滚操作，撤消撤消之前到事务开始时的所以操作。\n\n**事务的 ACID**\n\n事务具有四个特征：原子性（ Atomicity ）、一致性（ Consistency ）、隔离性（ Isolation ）和持续性（ Durability ）。这四个特性简称为 ACID 特性。\n\n> 1 、原子性。事务是数据库的逻辑工作单位，事务中包含的各操作要么都做，要么都不做\n> \n> 2 、一致性。事 务执行的结果必须是使数据库从一个一致性状态变到另一个一致性状态。因此当数据库只包含成功事务提交的结果时，就说数据库处于一致性状态。如果数据库系统 运行中发生故障，有些事务尚未完成就被迫中断，这些未完成事务对数据库所做的修改有一部分已写入物理数据库，这时数据库就处于一种不正确的状态，或者说是 不一致的状态。',
-        now(),now(), '0');
+VALUES ('2', '1', '真正理解Mysql的四种隔离级别@', '事务是应用程序中一系列严密的操作，所有操作必须成功完成，否则在每个操作中所作的所有更改都会被撤消。也就是事务具有原子性，一个事务中的一系列的操作要么全部成功，要么一个都不做。\n\n事务的结束有两种，当事务中的所以步骤全部成功执行时，事务提交。如果其中一个步骤失败，将发生回滚操作，撤消撤消之前到事务开始时的所以操作 ','2',now(),now(), '0','0','0','0','0');
+
+INSERT INTO `m_blog_content` VALUES ('2', '### 什么是事务  \n\n> 事务是应用程序中一系列严密的操作，所有操作必须成功完成，否则在每个操作中所作的所有更改都会被撤消。也就是事务具有原子性，一个事务中的一系列的操作要么全部成功，要么一个都不做。\n> \n> 事务的结束有两种，当事务中的所以步骤全部成功执行时，事务提交。如果其中一个步骤失败，将发生回滚操作，撤消撤消之前到事务开始时的所以操作。\n\n**事务的 ACID**\n\n事务具有四个特征：原子性（ Atomicity ）、一致性（ Consistency ）、隔离性（ Isolation ）和持续性（ Durability ）。这四个特性简称为 ACID 特性。\n\n> 1 、原子性。事务是数据库的逻辑工作单位，事务中包含的各操作要么都做，要么都不做\n> \n> 2 、一致性。事 务执行的结果必须是使数据库从一个一致性状态变到另一个一致性状态。因此当数据库只包含成功事务提交的结果时，就说数据库处于一致性状态。如果数据库系统 运行中发生故障，有些事务尚未完成就被迫中断，这些未完成事务对数据库所做的修改有一部分已写入物理数据库，这时数据库就处于一种不正确的状态，或者说是 不一致的状态。', '2');
 
 -- ----------------------------
 -- Table structure for m_permission
@@ -55,13 +57,13 @@ CREATE TABLE `m_permission`
 -- ----------------------------
 BEGIN;
 INSERT INTO `m_permission`
-VALUES (1072806379288399872, '测试页面', '/test', 1, 'page:test', NULL, 1, 0);
+VALUES (1072806379288399872, '测试页面', '/test', 1, 'page:test', '', 1, 0);
 INSERT INTO `m_permission`
 VALUES (1072806379313565696, '测试页面-查询', '/**/test', 2, 'btn:test:query', 'GET', 1, 1072806379288399872);
 INSERT INTO `m_permission`
 VALUES (1072806379330342912, '测试页面-添加', '/**/test', 2, 'btn:test:insert', 'POST', 2, 1072806379288399872);
 INSERT INTO `m_permission`
-VALUES (1072806379342925824, '监控在线用户页面', '/monitor', 1, 'page:monitor:online', NULL, 2, 0);
+VALUES (1072806379342925824, '监控在线用户页面', '/monitor', 1, 'page:monitor:online', '', 2, 0);
 INSERT INTO `m_permission`
 VALUES (1072806379363897344, '在线用户页面-查询', '/**/api/monitor/online/user', 2, 'btn:monitor:online:query', 'GET', 1,
         1072806379342925824);
